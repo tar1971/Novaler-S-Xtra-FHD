ac22d0c0d33852c8dc7ed0525cac6273ee57135a
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-04-20 11:38:22 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -7,52 +7,6 @@ enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all"
 echo "******************************************************************************************************************"
 echo "    Download And Install Skins Novaler-S-Xtra-FHD  "
 echo "============================================================================================================================="
-#####################################################################################
-echo "         INSTALL skin.Novaler-S-Xtra-FHD-V5.2    "
-cd /tmp
-set -e 
-wget "$MY_URL/$skin.Novaler-S-Xtra-FHD"
-wait
-opkg install --force-overwrite /tmp/*.ipk
-wait
-cd ..
-set +e
-rm -f /tmp/$skin.Novaler-S-Xtra-FHD
-echo "==========================================================================================================================="
-echo "         INSTALL SKIN-NOVALER-XTRA-COOLER    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_COOLER"
-wait
-tar -xzf $NOVALER_XTRA_COOLER  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER_XTRA_COOLER
-echo "****************************************************************************************************************************"
-echo "       INSTALL  SKIN NOVALER-XTRA-MUSTARD    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_MUSTARD"
-wait
-tar -xzf $NOVALER_XTRA_MUSTARD  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER-XTRA-MUSTARD
-echo "****************************************************************************************************************************"
-echo "         Install SKIN-NOVALER-XTRA-TWILLIGHT    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_TWILLIGHT"
-wait
-tar -xzf $NOVALER_XTRA_TWILLIGHT  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER_XTRA_TWILLIGHT
-echo "==========================================================================================================================="
-echo "==========================================================================================================================="
 echo "# SKIN -S-XTRA NOVALER INSTALLED #"
 echo "
 echo " "*********************************************************" 
