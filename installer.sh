323f6dbe47cbdce7d8b409356ba83ee9efa49877
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-04-20 11:41:34 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -6,14 +6,10 @@ MY_URL="https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main"
 enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all"
 echo "******************************************************************************************************************"
 echo "    Download And Install Skins Novaler-S-Xtra-FHD  "
-echo "============================================================================================================================="
-echo "# SKIN -S-XTRA NOVALER INSTALLED #"
-echo "
-echo " "*********************************************************" 
-	echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   TAR_TAR "   
-sleep 4;
-	echo '========================================================================================================================='
+echo opkg remove enigma2-plugin-extensions-BootLogoSwapper 
+wait
+opkg install --force-overwrite "https://github.com/tar1971/Novaler-S-Xtra-FHD/blob/main/enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all.ipk"
+wait
 ###########################################                                                                                                                  
 echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
