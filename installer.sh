192a981cc590841de094af2008b9f1c0de9d97c5
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-04-20 11:46:03 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -5,7 +5,6 @@ MY_URL="https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main"
 ########################################  
 
 echo "    Download And Install Skins Novaler-S-Xtra-FHD  "
-wait
 opkg install --force-overwrite "https://github.com/tar1971/Novaler-S-Xtra-FHD/raw/main/enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all.ipk"
 wait
 ###########################################                                                                                                                  
