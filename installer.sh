eed55ff8c8f5c65e4a565f9d84db1221624e5489
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-04-22 16:10:08 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 # 
 ## Command: wget https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL="https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main"
+MY_URL="https://github.com/tar1971/Novaler-S-Xtra-FHD/blob/main/enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all.ipk?raw=true/tar1971/Novaler-S-Xtra-FHD/main"
 ########################################  
 Download And Install Skins Novaler-S-Xtra-FHD  "
 opkg install --force-overwrite "https://github.com/tar1971/Novaler-S-Xtra-FHD/blob/main/enigma2-plugin_skin.Novaler-S-Xtra-FHD-V5.2_all.ipk?raw=true"
