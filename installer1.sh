0b482a4b4d887d63f030f20931d2249a7385eccd
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-21 04:15:37 +0200

    Update installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -3,12 +3,9 @@
 ## Command: wget https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main/installer1.sh -qO - | /bin/sh # # ########################################### ###########################################  
 MY_URL="https://raw.githubusercontent.com/tar1971/Novaler-S-Xtra-FHD/main"
 ########################################  
-NOVALLER_XTRA_BLUE="Novaler-S-X-FHD-Blue.tar"
-NOVALER_XTRA_COOLER="Novaler-S-Xtra-Cooler.tar.gz"
-NOVALER_XTRA_MUSTARD="Novaler-S-Xtra-Mustard.tar.gz"
-NOVALER_XTRA_TWILLIGHT="Novaler-S-Xtra-Twillight.tar.gz"
+NOVALLER_XTRA_BLUE="Novaler-S-Xtra-FHD"
 echo "******************************************************************************************************************"
-echo "    Download And Install Skins Novaler-Xtra  "
+echo "    Download And Install Skins Novaler-S-Xtra-FHD  "
 echo "============================================================================================================================="
 #####################################################################################
 echo "         INSTALL NOVALER-S-X-FHD-BLUE    "
@@ -21,40 +18,6 @@ wait
 cd ..
 set +e
 rm -f /tmp/$NOVALER-S-X-FHD-BLUE
-echo "==========================================================================================================================="
-echo "         INSTALL SKIN-NOVALER-XTRA-COOLER    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_COOLER"
-wait
-tar -xzf $NOVALER_XTRA_COOLER  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER_XTRA_COOLER
-echo "****************************************************************************************************************************"
-echo "       INSTALL  SKIN NOVALER-XTRA-MUSTARD    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_MUSTARD"
-wait
-tar -xzf $NOVALER_XTRA_MUSTARD  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER-XTRA-MUSTARD
-echo "****************************************************************************************************************************"
-echo "         Install SKIN-NOVALER-XTRA-TWILLIGHT    "
-cd /tmp
-set -e 
-wget "$MY_URL/$NOVALER_XTRA_TWILLIGHT"
-wait
-tar -xzf $NOVALER_XTRA_TWILLIGHT  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/$NOVALER_XTRA_TWILLIGHT
-echo "==========================================================================================================================="
 echo "==========================================================================================================================="
 echo "# SKIN -S-XTRA NOVALER INSTALLED #"
 echo "
